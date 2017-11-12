5de103070e983a09189448fa68adf100aee7561c
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-11-12 11:11:34 +0900

    reduce too much request on scroll

--- render/index.js
+++ render/index.js
@@ -6,26 +6,34 @@ import Contents from './Contents.vue'
 var client;
 var dashboard = undefined;
 
+var scrollTimeOut;
 ipcRenderer.on('dashboard', (evt, msg) => {
 	if (dashboard === undefined) {
 		dashboard = msg;
-		renderDashboard(dashboard);
+		render(dashboard);
 		var sidebar = document.getElementById('sidebar');
 		sidebar.onscroll = function() {
-			if (this.scrollHeight - this.scrollTop === this.clientHeight) {
-				ipcRenderer.send('get', {before_id: dashboard.posts[dashboard.posts.length-1].id});
-			}
+			clearTimeout(scrollTimeOut)
+			scrollTimeOut = setTimeout(function() {
+				if (sidebar.scrollHeight - sidebar.scrollTop - sidebar.clientHeight  < 100) {
+					getNextDashboard();
+				}
+			}, 200)
 		}
 	} else {
 		dashboard.posts = dashboard.posts.concat(msg.posts);
 	}
 });
 
-function renderDashboard(obj) {
+function render(obj) {
 	var side = new Vue({
 		el: '#sidebar',
 		data: function() { return obj},
 		template: "<SideBar v-bind:posts='posts' />",
 		components: {SideBar}
 	});
+}
+
+function getNextDashboard() {
+	ipcRenderer.send('get', {before_id: dashboard.posts[dashboard.posts.length-1].id});
 }
