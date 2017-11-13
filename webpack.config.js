103e35a0d3ea179fbd8e4176dfb6a17281bc2ca4
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-11-13 21:30:06 +0900

    Use scss insted of css

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = [
   {
     entry: './render/index.js',
@@ -23,16 +24,24 @@ module.exports = [
     target: 'electron'
   },
   {
-    entry: {style: './render/index.scss'},
+    entry: {
+      style: './render/index.scss'
+    },
     output: {
       path: __dirname+'/render',
-      filename: 'bundle.css'
+      filename: '[name].css'
     },
     module: {
       rules: [{
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!sass-loader',
+        })
       }]
-    }
+    },
+    plugins: [
+      new ExtractTextPlugin("[name].css")
+    ]
   }
 ];
