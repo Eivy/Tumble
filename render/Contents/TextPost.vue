b71389c941119b49c68140c8787f8ac02a9c148f
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-11-24 17:34:14 +0900

    fix

--- render/Contents/TextPost.vue
+++ render/Contents/TextPost.vue
@@ -1,17 +1,43 @@
 <template>
-	<div class='text' ><h4>{{post.title}}</h4><div>{{sanitizeText}}</div></div>
+	<div class='text' >
+		<h4>{{post.title}}</h4>
+		<div v-html=post.body ></div>
+		<div>
+			<PostInfo v-bind:post=post />
+			<Tag v-if='post.tags.length > 0' v-bind:post=post />
+		</div>
+	</div>
 </template>
 
 <script>
 import Mixin from './Mixin.vue'
 export default {
-	mixins: [Mixin],
-	computed: {
-		sanitizeText: function() {
-			var e = document.createElement('div');
-			e.innerHTML = this.post.body;
-			return e.textContent;
-		}
-	}
+	mixins: [Mixin]
 }
 </script>
+
+<style scoped lang='scss'>
+@import '../base.scss';
+.text {
+	background-color: #fff;
+	h4 {
+		margin: 0;
+	}
+}
+.text /deep/ .info {
+	background-color: $backgroundColor;
+}
+.text /deep/ .info div {
+	color: #fff;
+	border-bottom: 1px solid #888;
+	margin-left: 50px;
+	min-height: 30px;
+	line-height: 30px;
+}
+.text /deep/ .info .separator {
+	margin: 1px 0px 1px 0px;
+	background-color: #56667d;
+	min-height: 20px;
+	line-height: 20px;
+}
+</style>
