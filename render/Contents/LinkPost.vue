95d260f61d7b9b2a27b951bdf98942478e81e268
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-11-26 23:25:31 +0900

    Change Link post

--- render/Contents/LinkPost.vue
+++ render/Contents/LinkPost.vue
@@ -1,5 +1,16 @@
 <template>
-	<div class='link' >{{post.title}}</div>
+	<div>
+		<div class='link' >
+			<a :href=post.url>{{post.title}}</a>
+			<div>{{post.excerpt}}</div>
+			<img v-for="photo in post.photos" :src=photo.original_size.url>
+			<div v-html=post.description ></div>
+		</div>
+		<div>
+			<PostInfo v-bind:post=post />
+			<PostTag v-if='post.tags.length > 0' v-bind:post=post />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -8,3 +19,15 @@ export default {
 	mixins: [Mixin]
 }
 </script>
+
+<style scoped lang='scss'>
+@import '../base.scss';
+.link {
+	background-color: #fff;
+	padding: $contentPadding;
+	img {
+		width: 100%;
+		vertical-align: bottom;
+	}
+}
+</style>
