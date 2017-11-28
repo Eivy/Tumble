b06b70a09bd38ce5c2fd5dcf635b19fc40ed902c
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-11-28 17:36:08 +0900

    Change Video post structure

--- render/Contents/VideoPost.vue
+++ render/Contents/VideoPost.vue
@@ -1,10 +1,30 @@
 <template>
-	<div class='video' >{{post.blog_name}}<br/>Video</div>
+	<div>
+		<div class='video' v-html=post.player[post.player.length-1].embed_code />
+		<div
+		<div>
+			<PostCaption v-if='post.caption.length > 0' v-bind:post=post />
+			<PostInfo v-bind:post=post />
+			<PostTag v-if='post.tags.length > 0' v-bind:post=post />
+		</div>
+	</div>
 </template>
 
 <script>
 import Mixin from './Mixin.vue'
 export default {
-	mixins: [Mixin]
+	mixins: [Mixin],
+	mounted: function() {
+		var video = document.getElementsByTagName('video')[0];
+		video.setAttribute('controls', '');
+		video.setAttribute('preload', 'metadata');
+		video.removeAttribute('muted', '');
+	}
 }
 </script>
+
+<style scoped lang='scss'>
+.video {
+	text-align: center;
+}
+</style>
