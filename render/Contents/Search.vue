03de7f4486cac7ac9b213cbea279ef3da55470d8
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-12-20 14:09:30 +0900

    Add search page

---
+++ render/Contents/Search.vue
@@ -0,0 +1,65 @@
+<template>
+	<div id='contents'>
+		<div id='search'>
+			<Icon name='search'></Icon>
+			<input type='search' @search=search($event.target.value) ></input>
+		</div>
+		<div id='log'>
+			<LinkItem v-for='(l, i) in log' :key=i :text=l @click.native=search(l)></LinkItem>
+		</div>
+	</div>
+</template>
+
+<script>
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon.vue'
+import Store from 'electron-store'
+import LinkItem from '../LinkItem.vue'
+const store = new Store()
+export default {
+	components: {Icon, LinkItem},
+	data: function () { return {'log': store.get('cache.search', [])}},
+	methods: {
+		search: function(value) {
+			if (value.length > 0) {
+				this.$router.push('/tag/'+value)
+				event.target.blur();
+				this.log.unshift(value)
+				this.log = this.log.filter((item, i, array) => array.indexOf(item) == i)
+				store.set('cache.search', this.log);
+			}
+		}
+	}
+}
+</script>
+
+<style scoped lang='scss'>
+@import '../base.scss';
+#search {
+	@include header;
+	input {
+		position: absolute;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		padding-left: $headerHeight / 2;
+		background-clip: padding-box;
+		border-radius: 5px;
+		border: 5px solid $backgroundColor;
+		box-sizing: border-box;
+		font-size: 100%;
+	}
+	svg {
+		position: absolute;
+		left: 0;
+		width: $headerHeight / 4;
+		height: 100%;
+		padding-left: $headerHeight / 4;
+		color: #000;
+		z-index: 99;
+	}
+}
+#log {
+	@include content;
+}
+</style>
--- render/Links.vue
+++ render/Links.vue
@@ -2,18 +2,29 @@
 	<div id='links'>
 		<div id='dashboard' @click="goTo('/dashboard')"><Icon name='dashboard'></Icon></div>
 		<div id='likes' @click="goTo('/likes')"><Icon name='heartbeat'></Icon></div>
+		<div id='search' @click="search"><Icon name='search'></Icon></div>
 		<div id='userinfo' @click="goTo('/userinfo')"><Icon name='user'></Icon></div>
 	</div>
 </template>
 
 <script>
+import Vue from 'vue'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon.vue'
+import Search from './Contents/Search.vue'
 export default {
 	components: {Icon},
 	methods: {
 		goTo: function(target) {
 			this.$router.push(target);
+		},
+		search: function() {
+			new Vue({
+				el: '#contents',
+				components: {Search},
+				template: '<Search></Search>',
+				router: this.$router
+			})
 		}
 	}
 }
@@ -28,7 +39,7 @@ export default {
 		display: inline-block;
 		height: $headerHeight;
 		width: $headerHeight / 2;
-		padding: 0 ($headerHeight/2) 0 ($headerHeight/2);
+		padding: 0 1em 0 1em;
 		svg {
 			width: 100%;
 			height: 100%;
