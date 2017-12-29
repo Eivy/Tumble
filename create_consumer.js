bf3c743f6a1b98978dc7a25347248cdc26cc6c6a
Eivy <modern.times.rock.and.roll+git@gmail.com>
2017-12-29 12:08:11 +0900

    Fix consumer creating script

--- create_consumer.js
+++ create_consumer.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 prompt.start();
 prompt.get(['key', 'secret'], (err, result) => {
 	var o = {
-		'key': result.key,
-		'secret': result.secret
+		'consumer_key': result.key,
+		'consumer_secret': result.secret
 	};
 	fs.writeFile('main/consumer.js', 'export default '+JSON.stringify(o), (err) => {
 		if (err) {
