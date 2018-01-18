9bb2f1fb3144049170019f3115a2bd223d7f5c3e
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-01-18 16:53:53 +0900

    Change creating consumer for CI

--- create_consumer.js
+++ create_consumer.js
@@ -1,12 +1,28 @@
 var prompt = require('prompt');
 var fs = require('fs');
+var consumer = {};
+var prompting = [];
+if (process.env.CONSUMER_KEY !== undefined) {
+	consumer.consumer_key = process.env.CONSUMER_KEY;
+} else {
+	prompting.push('key');
+}
+if (process.env.CONSUMER_SECRET !== undefined) {
+	consumer.consumer_secret = process.env.CONSUMER_SECRET;
+} else {
+	prompting.push('secret');
+}
 prompt.start();
-prompt.get(['key', 'secret'], (err, result) => {
-	var o = {
-		'consumer_key': result.key,
-		'consumer_secret': result.secret
-	};
-	fs.writeFile('main/consumer.js', 'export default '+JSON.stringify(o), (err) => {
+prompt.get(prompting, (err, result) => {
+	let o = {};
+	if (result.key != undefined) {
+		o.consumer_key = result.key;
+	}
+	if (result.secret != undefined) {
+		o.consumer_secret = result.secret;
+	}
+	Object.assign(consumer, o);
+	fs.writeFile('main/consumer.js', 'export default '+JSON.stringify(consumer), (err) => {
 		if (err) {
 			console.log('Error!! '+err);
 			process.exit(1);
